50403ec0f1c5ccf9c0d93ad7747fc5c195bdd0c7
hoangmanhkhiem <khiemhm04@gmail.com>
2024-12-24 06:01:47 +0700

    up

---
+++ gunicorn_config.py
@@ -0,0 +1,5 @@
+bind = "0.0.0.0:8000"       # Set the host and port
+workers = 4                 # Number of worker processes, adjust based on your CPU cores
+threads = 2                 # Number of threads per worker
+timeout = 120               # Timeout in seconds
+loglevel = "info"           # Logging level
